f045ad065a5bf11619c694589a53c59c4b88bdc8
Anand kushwaha <anandkushwaha70@gmail.com>
2023-02-11 01:16:35 +0530

    Assignment

--- App.js
+++ App.js
@@ -1,26 +1,49 @@
 import React from "react";
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "heading 1",
-  },
-  "Heading 1"
-);
-const heading1 = React.createElement(
-  "h2",
-  {
-    id: "heading 2",
-  },
-  "Heading 2"
-);
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading1]
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "heading 1",
+//     key:"h1"
+//   },
+//   "Heading 1"
+// );
+// const heading1 = React.createElement(
+//   "h2",
+//   {
+//     id: "heading 2",
+//     key:"h2"
+//   },
+//   "Heading 2"
+// );
+// const container = React.createElement(
+//   "div",
+//   {
+//     id: "container",
+//   },
+//   [heading, heading1]
+// );
+
+//--------jsx-----//
+const heading = (
+  <h1 id="title" key="h1">
+    {" "}
+    Namaste React
+  </h1>
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+
+/// Component
+
+const HeaderCompontent = () => {
+  return (
+    <div>
+      {heading}
+      <h1>Namaste React Functional Component</h1>
+      <h1>This h1 tag</h1>
+    </div>
+  );
+};
+root.render(<HeaderCompontent/>);
+// root.render(heading);
